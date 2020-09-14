7419411c09cc0f7fed8703261d020eb41833c355
Vadim <vadim.kirillov2001@gmail.com>
2020-09-14 11:48:15 +0300

    Initial commit

---
+++ src/MovablePoint.java
@@ -0,0 +1,41 @@
+public class MovablePoint implements Movable {
+    protected int x,y,xSpeed,ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "x=" + x +
+                ", y=" + y +
+                ", xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+
+    @Override
+    public void moveUp() {
+        this.x += xSpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        this.x -= xSpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        this.y += ySpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        this.y -= ySpeed;
+    }
+
+}
